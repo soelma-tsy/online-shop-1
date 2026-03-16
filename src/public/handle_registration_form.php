fec3571039ecbef820c4207e2b9819ae6e51d5c7
Soelma Tsy <soelmatsy23122013@icloud.com>
2026-03-16 20:55:18 +0900

    валидация

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -1,12 +1,57 @@
 <?php
 
-print_r($_GET);
+if (empty($_GET)) {
+    exit("Пожалуйста, заполните форму регистрации.");
+}
 
 $name = $_GET['name'];
 $email = $_GET['email'];
 $password = $_GET['psw'];
-$passwordRepeat = $_GET['psw-repeat'];
+$passwordRep = $_GET['psw_repeat'];
 
-$pdo = new PDO('pgsql:host=db;port=5432;dbname=mydb', 'user', 'pass');
+$errors = [];
 
-$pdo->exec("INSERT INTO users (name, email, password, passwordRepeat) VALUES ('name', 'email', 'psw', 'psw-repeat')");
+if (strlen($name) < 2) {
+    $errors[] = 'Имя должно содержать более 2 символов' . "<br>";
+}
+
+if (strlen($email) < 3) {
+    $errors[] = 'email должен содержать более 3 символов' . "<br>";
+}
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    $errors[] = 'email введен некорректно, пожалуйста, проверьте правильность ввода' . "<br>";
+}
+
+if (strlen($password) < 6) {
+    $errors[] = 'Пароль должен содержать более 6 символов' . "<br>";
+}
+
+if ($password != $passwordRep) {
+    $errors[] = 'Пароли не совпадают, повторите попытку' . "<br>";
+}
+
+if (empty($name) || empty($email) || empty($password)) {
+    $errors[] = 'Все поля должны быть заполнены!' . "<br>";
+}
+
+if (empty($errors)) {
+    $pdo = new PDO('pgsql:host=db;port=5432;dbname=mydb', 'user', 'pass');
+    $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$name, $email, $password]);
+    echo "<h2>Регистрация прошла успешно! Добро пожаловать, $name!</h2>";
+    echo "Ваше имя: " . $name . "<br>";
+    echo "Ваш email: " . $email;
+} else {
+    echo "Ошибка! Проверьте правильность введенных вами данных и повторите попытку";
+    foreach ($errors as $error) {
+        echo $error . "<br>";
+    }
+}
+
+
+
+
+
+
--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,7 @@
 
 $pdo = new PDO('pgsql:host=db;port=5432;dbname=mydb', 'user', 'pass');
 
-$pdo->exec("INSERT INTO users (name, email, password) VALUES ('IVAN', 'ivan@mail.ru', 'qwerty123')");
+$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
 
 $statement = $pdo->query("SELECT * FROM users");
 
@@ -11,6 +11,5 @@ echo "<pre>";
 
 print_r($data);
 echo "<pre>";
-
 
 
